6791e958512817142503b6c6560179dbdab9b943
ccastillo32 <cristiancastillo32@gmail.com>
2024-02-02 11:42:13 -0500

    Bootstrap added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import './main.css'
+
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
+  <Home />,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export function Home () {
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      Home
+    </div>
+  )
+}
