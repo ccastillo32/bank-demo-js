9bc21909997704e3042541ed5330f964607faaa8
ccastillo32 <cristiancastillo32@gmail.com>
2024-02-05 21:40:25 -0500

    inputs added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,13 +1,48 @@
+import './register.css'
+
 export function Register () {
   return (
     <div className="d-flex align-items-center justify-content-center page-container">
-      <div className="card registration-form-card">
+
+      {/* Registration form */}
+      <div className="card registration-form-card col-6 bg-transparent border-0">
         <div className="card-body">
-          <h1 className="form-header card-title">
+
+          {/* Header */}
+          <h1 className="form-header card-title mb-3">
             <i className="fa fa-edit"></i> Register
           </h1>
+
+          {/* Form */}
+          <form action="" className="registration-form">
+            <div className="row">
+              <div className="form-group col">
+                <input type="text" name="first-name" className="form-control form-control-lg" placeholder="Enter first name" />
+              </div>
+
+              <div className="form-group col">
+                <input type="text" name="last-name" className="form-control form-control-lg" placeholder="Enter last name" />
+              </div>
+            </div>
+
+            <div className="form-group col">
+              <input type="email" name="email" className="form-control form-control-lg" placeholder="Enter email" />
+            </div>
+
+            <div className="row">
+              <div className="form-group col">
+                <input type="password" name="first-name" className="form-control form-control-lg" placeholder="Enter password" />
+              </div>
+
+              <div className="form-group col">
+                <input type="password" name="last-name" className="form-control form-control-lg   " placeholder="Confirm password" />
+              </div>
+            </div>
+          </form>
+
         </div>
       </div>
+
     </div>
   )
 }
